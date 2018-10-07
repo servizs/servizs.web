64387cfc05d8a390846a1e82a086965ae0d6bca6
servizs <admin@servizs.com>
2018-10-07 15:06:47 -0400

    feat: successfuly generated token

--- src/app/stripe/components/smart/payment-details-container.component.ts
+++ src/app/stripe/components/smart/payment-details-container.component.ts
@@ -1,26 +1,78 @@
 import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import { PaymentProviderService } from '../../payment-provider.service';
 
+const style = {
+  base: {
+    iconColor: '#666ee8',
+    color: '#31325f',
+    fontWeight: 400,
+    fontFamily:
+      '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif',
+    fontSmoothing: 'antialiased',
+    fontSize: '15px',
+    '::placeholder': {
+      color: '#aab7c4'
+    },
+    ':-webkit-autofill': {
+      color: '#666ee8'
+    }
+  }
+};
+
 @Component({
   selector: 'app-payment-details-container',
   templateUrl: './payment-details-container.component.html',
   styleUrls: ['./payment-details-container.component.css']
 })
 export class PaymentDetailsContainerComponent implements AfterViewInit, OnDestroy {
-  @ViewChild('cardInfo')
-  cardInfo: ElementRef;
+  @ViewChild('cardElement')
+  cardElement: ElementRef;
+
+  @ViewChild('cardErrors')
+  cardErrors: ElementRef;
+
+  @ViewChild('ibanErrors')
+  ibanErrors: ElementRef;
+
+  @ViewChild('submitButton')
+  submitButton: ElementRef;
+
+  @ViewChild('ibanElement')
+  bankElement: ElementRef;
+
+  @ViewChild('paymentForm')
+  paymentForm: ElementRef;
 
   card: any;
-  cardHandler = this.onChange.bind(this);
+  cardHandler = this.onCardChange.bind(this);
+  bankHandler = this.onBankChange.bind(this);
+  formSubmissionHandler = this.onFormSubmit.bind(this);
   error: string;
 
   constructor(private cd: ChangeDetectorRef, private readonly paymentProviderService: PaymentProviderService) {}
 
   ngAfterViewInit() {
-    this.card = this.paymentProviderService.elements.create('card');
-    this.card.mount(this.cardInfo.nativeElement);
+    this.card = this.paymentProviderService.elements.create('card', { style });
+    this.card.mount(this.cardElement.nativeElement);
 
     this.card.addEventListener('change', this.cardHandler);
+    (this.paymentForm.nativeElement as HTMLElement).addEventListener('submit', this.formSubmissionHandler);
+  }
+
+  private onFormSubmit(event) {
+    event.preventDefault();
+
+    this.paymentProviderService.stripe.createToken(this.card).then(function(result) {
+      if (result.error) {
+        // Inform the user if there was an error.
+        var errorElement = document.getElementById('card-errors');
+        errorElement.textContent = result.error.message;
+      } else {
+        // Send the token to your server.
+        // stripeTokenHandler(result.token);
+        debugger;
+      }
+    });
   }
 
   ngOnDestroy() {
@@ -28,12 +80,17 @@ export class PaymentDetailsContainerComponent implements AfterViewInit, OnDestro
     this.card.destroy();
   }
 
-  onChange({ error }) {
+  onCardChange({ error }) {
+    const errors = this.cardErrors.nativeElement as HTMLElement;
     if (error) {
-      this.error = error.message;
+      errors.textContent = error.message;
+      errors.classList.add('visible');
     } else {
-      this.error = null;
+      error = null;
+      errors.classList.remove('visible');
     }
+
+    this.submitButton.nativeElement.disabled = false;
     this.cd.detectChanges();
   }
 
@@ -46,5 +103,32 @@ export class PaymentDetailsContainerComponent implements AfterViewInit, OnDestro
       console.log('Success!', token);
       // ...send the token to the your backend to process the charge
     }
+  }
+
+  private setUpBankElement() {
+    const ibanOptions = {
+      style,
+      supportedCountries: ['SEPA']
+    };
+    const iban = this.paymentProviderService.elements.create('iban', ibanOptions);
+    iban.mount(this.bankElement.nativeElement);
+  }
+
+  onBankChange({ error, bankName }) {
+    const errors = this.ibanErrors.nativeElement as HTMLElement;
+    if (error) {
+      errors.textContent = error.message;
+      errors.classList.add('visible');
+    } else {
+      error = null;
+      errors.classList.remove('visible');
+
+      /*if (bankName) {
+        updateButtonLabel('sepa_debit', bankName);
+      }*/
+    }
+
+    this.submitButton.nativeElement.disabled = false;
+    this.cd.detectChanges();
   }
 }
